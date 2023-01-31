6dbc9585769f474c77c055bddb3c55b283998335
Gökhan Eren Kamer <gek@eskisehir.edu.tr>
2023-01-31 21:52:01 +0300

    Gun01 Dersleri tamamlandı. Selenium lib. yüklendi. ilk kod tamamlanarak chrome ve safari de techno study sayfası açıldığı görüldü.

--- src/Gun01/MainApp.java
+++ src/Gun01/MainApp.java
@@ -1,7 +1,17 @@
 package Gun01;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class MainApp {
     public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        //WebDriver driver1 = new EdgeDriver();
+        WebDriver driver2 = new SafariDriver();
+        driver.get("https://www.techno.study/");
+        //driver1.get("https://www.techno.study/");
+        driver2.get("https://www.techno.study/");
 
     }
 }
